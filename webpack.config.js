d23d807909c2f7b6ce925eb3a86410141769c229
Nyle-Blake <Nyleblake1@gmail.com>
2024-03-18 00:49:25 +0000

    inital commit. webpack, html, css, empty index.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: "Restaurant Page",
+  })
+  ],
+};
