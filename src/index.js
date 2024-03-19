3898e315c34b22a478584c4e0b17d2673c8855fc
Nyle-Blake <Nyleblake1@gmail.com>
2024-03-19 15:37:19 +0000

    Finish module_main.js file which creates the page layout

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import './style.css';
-import { createMain } from './module_main.js';
-import { createMenu } from './module_menu.js;'
-import { createContacts } from './module_contacts.js';
 
-const mainbtn = document.querySelector('#main');
+import { createMain } from './module_main.js';
+// import { createMenu } from './module_menu.js;';
+// import { createContacts } from './module_contacts.js';
+
+const mainBtn = document.querySelector('#main');
 const menuBtn = document.querySelector('#menu');
 const contactsBtn = document.querySelector('#contacts');
 
+mainBtn.addEventListener('click', () => {
+  createMain
+})
+
+// menuBtn.addEventListener('click', () => {
+//   createMenu
+// })
+
+// contactsBtn.addEventListener('click', () => {
+//   createContacts
+// })
+
+createMain;
--- src/module_main.js
+++ src/module_main.js
@@ -1,5 +1,50 @@
 const createMain = () => {
-  document.appendChild()
+
+  const container = document.createElement('div')
+  container.classList.add('container')
+
+  const logo = document.createElement('h1')
+  logo.textContent = 'The Honey Badger'
+
+  const btn1 = document.createElement('button')
+  btn1.textContent = 'Main'
+
+  const btn2 = document.createElement('button')
+  btn2.textContent = 'Menu'
+
+  const btn3 = document.createElement('button')
+  btn3.textContent = 'Contacts'
+
+  container
+    .appendChild(document.createElement('header'))
+    .appendChild(logo)
+    .insertAdjacentElement('afterend', document.createElement('nav'))
+    .appendChild(btn1)
+    .insertAdjacentElement(btn2)
+    .insertAdjacentElement(btn3)
+
+  const box1 = document.createElement('div')
+  const box2 = document.createElement('div')
+  box1.classList.add('box1')
+  box2.classList.add('box2')
+
+  box1
+    .appendChild(document.createElement('h3'))
+    .insertAdjacentElement('afterend', document.createElement('p'))
+  
+  box2
+    .appendChild(document.createElement('h3'))
+    .insertAdjacentElement('afterend', document.createElement('p'))
+
+  container
+    .appendChild(document.createElement('main'))
+    .appendChild(box1)
+    .insertAdjacentElement('afterend', box2)
+  
+  document.appendChild(container)
+
+  return;
+
 }
 
-export { createMain as default }
+export { createMain }
